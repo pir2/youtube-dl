8826ec2e54cfabeba862577135618b6ea12aafa8
pir2 <pir2@users.noreply.github.com>
2019-11-05 13:23:44 -0500

    Update aznfns.py

--- youtube_dl/extractor/aznfns.py
+++ youtube_dl/extractor/aznfns.py
@@ -16,13 +16,11 @@ class BusinessInsiderIE(InfoExtractor):
     }]
 
     def _real_extract(self, url):
-        video_id = self._match_id(url)
+        mobj = re.match(self._VALID_URL, url)
+        video_id, display_id = mobj.groups()
+
         webpage = self._download_webpage(url, video_id)
-        jwplatform_id = self._search_regex(
-            (r'data-media-id=["\']([a-zA-Z0-9]{8})',
-             r'id=["\']jwplayer_([a-zA-Z0-9]{8})',
-             r'id["\']?\s*:\s*["\']?([a-zA-Z0-9]{8})'),
-            webpage, 'jwplatform id')
+        video_data = self._extract_jwplayer_data(webpage, video_id, require_title=False)
         return self.url_result(
             'jwplatform:%s' % jwplatform_id, ie=JWPlatformIE.ie_key(),
             video_id=video_id)
